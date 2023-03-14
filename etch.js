f16d794765686c046f9584ccd9b5ce0a2ab903bf
George Sotiropoulos <george@gsotworks.com>
2023-03-14 00:29:51 -0500

    Patch grid rows overflow and add grid resetting
    Fixed the issue of high-number grids overflowing past the square
    boundaries by using CSS border instead of margin, as well as wrapping
    all code in functions to regenerate new grids on every button press.

--- etch.js
+++ etch.js
@@ -1,15 +1,17 @@
-const GRIDSIZE = 25;
+const GRIDSIZE = 64;
 const insertDiv = document.getElementById('maingrid');
 
-const chButton = document.createElement('button');
-chButton.addEventListener('click', generate);
-chButton.innerText = 'GENERATE';
-insertDiv.appendChild(chButton);
+function init() {
+    const chButton = document.createElement('button'); // init code: create our button with a listener bound to our generate() function
+    chButton.addEventListener('click', generate);
+    chButton.innerText = 'GENERATE';
+    insertDiv.appendChild(chButton);
+}
 
 function generate() {
     let input;
 
-    while (typeof input !== 'number' || !input) { // ensure input converts to an actual number and not NaN
+    while (typeof input !== 'number' || !input) { // ensure user input converts to an actual number and not NaN
         input = prompt('Enter a new number size for the grid:');
         input = Number(input);
     }
@@ -17,12 +19,15 @@ function generate() {
     input = Math.round(input); // round number to an integer and cap it at 100
     let size = input < 100 ? input : 100;
 
-    for (let i = 1; i < size+1; i++) { // Create a set of size rows
+    document.getElementById('maingrid').innerText = ''; // delete existing grid and create new one
+    init();
+
+    for (let i = 1; i < size+1; i++) { // Create a set of `size` rows
     
         const row = document.createElement('div');
         row.classList.add('maingrid-row');
     
-        for (let j = 1; j < size+1; j++) { // ...with size items in each row
+        for (let j = 1; j < size+1; j++) { // ...with `size` items in each row
             const item = document.createElement('div');
             item.classList.add('maingrid-item');
             
@@ -30,12 +35,14 @@ function generate() {
                 once: true
             });
 
-            item.style.height = `${GRIDSIZE/size}rem`; // not ideal, but using inline styles to size grid items within GRIDSIZExGRIDSIZE
+            item.style.height = `${GRIDSIZE/size}rem`; // not ideal, but using inline styles to size grid items within `GRIDSIZE`x`GRIDSIZE`
             item.style.width = `${GRIDSIZE/size}rem`;
-    
+                
             row.appendChild(item);
         }
     
         insertDiv.appendChild(row);
     }
 }
+
+init();
