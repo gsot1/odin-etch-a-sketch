fa04074288d26cfa46747c0369dc50c2dbf759ae
George Sotiropoulos <george@gsotworks.com>
2023-03-13 23:29:19 -0500

    Add hover state changes to grid squares
    Introduced an operation in the inner forloop that adds a single-use
    'mouseover' event listener to every box.

--- etch.js
+++ etch.js
@@ -3,10 +3,20 @@ const SIZE = 4;
 const insertDiv = document.getElementById('maingrid');
 
 for (let i = 1; i < SIZE+1; i++) {
+    
     const row = document.createElement('div');
     row.classList.add('maingrid-row');
+
     for (let j = 1; j < SIZE+1; j++) {
-        row.insertAdjacentHTML('beforeend', `<div class="maingrid-item"></div>`)
+        const item = document.createElement('div');
+        item.classList.add('maingrid-item');
+        
+        item.addEventListener('mouseover', () => item.classList.add('mg-item__hovered'), {
+            once: true
+        });
+
+        row.appendChild(item);
     }
+
     insertDiv.appendChild(row);
 }
